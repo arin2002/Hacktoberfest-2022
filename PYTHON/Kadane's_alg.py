987c13e4e055ee48fea4e5788786bf9a473a0a69
MrJayendra <114435795+MrJayendra@users.noreply.github.com>
2022-10-15 22:34:47 +0530

    Update Kadane's_alg.py

--- PYTHON/Kadane's_alg.py
+++ PYTHON/Kadane's_alg.py
@@ -8,7 +8,7 @@ def maxSubArraySum(a, size):
 		max_ending_here = max_ending_here + a[i]
 		if (max_so_far < max_ending_here):
 			max_so_far = max_ending_here
-		if max_ending_here < 0:
+		if( max_ending_here < 0):
 			max_ending_here = 0
 	return max_so_far
 
