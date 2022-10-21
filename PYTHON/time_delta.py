ea22c1445c43a95b0d0003bb060ae26329ed2b96
Giovanni Almeida <gronniea@gmail.com>
2022-10-21 09:37:55 -0300

    feat: time delta

---
+++ PYTHON/time_delta.py
@@ -0,0 +1,7 @@
+from datetime import datetime as dt
+
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for _ in range(int(input())):
+    time1 = dt.strptime(input(), fmt)
+    time2 = dt.strptime(input(), fmt)
+    print(int(abs((time1 - time2).total_seconds())))
