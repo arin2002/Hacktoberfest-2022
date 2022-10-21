81b49a302e3b98a1ef1a894dce239b868d18e8ff
Giovanni Almeida <gronniea@gmail.com>
2022-10-21 09:36:45 -0300

    feat: piling up

---
+++ PYTHON/piling_up.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+for i in range(int(input())):
+    _, n = input(), deque(map(int, input().split()))
+    ans = True
+
+    for j in range(len(n) - 1):
+        if n[0] >= n[1]:
+            n.popleft()
+        elif n[-1] >= n[-2]:
+            n.pop()
+        else:
+            ans = False
+            break
+
+    print('Yes' if ans else 'No')
