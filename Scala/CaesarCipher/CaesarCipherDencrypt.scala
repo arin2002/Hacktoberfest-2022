205ca50ae696b68dcd7c0cdb00a535a8ac8b8d6e
raminduw200 <raminduw200@gmail.com>
2022-10-23 14:27:22 +0530

    Create CaesarCipher encrypt and decrypt

---
+++ Scala/CaesarCipher/CaesarCipherDencrypt.scala
@@ -0,0 +1,22 @@
+object CaesarCipherDencrypt {
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    def main(args: Array[String]): Unit = {
+        print("Enter Shift: ");
+        var shift = (scala.io.StdIn.readInt() + alphabet.length) % alphabet.length;
+
+        var mssg = scala.io.StdIn.readLine("Enter Encrypted Message: ");
+        var denc = decrypt(mssg, shift);
+        println("Decrypt Message: " + denc);
+    }
+    
+    def encrypt(mssg: String, shift: Int): String = {
+        mssg.map((c: Char) => {
+            var indx = alphabet.indexOf(c.toUpper);
+            if(indx == -1)
+                c
+            else
+                alphabet((indx + shift) % alphabet.length);
+        });
+    }
+}
