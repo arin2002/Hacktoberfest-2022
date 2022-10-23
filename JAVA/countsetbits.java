51f597403f0c8bbf0895879a161cc020bdd0b4c3
Alok Singh <mailbox.alok.singh.11@gmail.com>
2022-10-23 21:14:53 +0530

    Added a program to count set bits

---
+++ JAVA/countsetbits.java
@@ -0,0 +1,15 @@
+public class countsetbits{
+    public static int countset(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        System.out.println(countset(15));
+    }
+}
