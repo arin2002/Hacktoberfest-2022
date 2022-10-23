3f18534d872579ba87fe3eb229541ba222f0105b
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-23 14:14:22 +0530

    Update binary_search.py

--- PYTHON/binary_search.py
+++ PYTHON/binary_search.py
@@ -1,29 +1,42 @@
-def binary_search(arr, low, high, x):
- 
-    if high >= low:
- 
-        mid = (high + low) // 2
+# Python3 Program for recursive binary search.
 
-        if arr[mid] == x:
-            return mid
+# Returns index of x in arr if present, else -1
 
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
 
-        else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        return -1
- 
-# Test array
-arr = [ 2, 3, 4, 10, 40 ]
-x = 3
- 
+def binarySearch(arr, l, r, x):
+
+	# Check base case
+	if r >= l:
+
+		mid = l + (r - l) // 2
+
+		# If element is present at the middle itself
+		if arr[mid] == x:
+			return mid
+
+		# If element is smaller than mid, then it
+		# can only be present in left subarray
+		elif arr[mid] > x:
+			return binarySearch(arr, l, mid-1, x)
+
+		# Else the element can only be present
+		# in right subarray
+		else:
+			return binarySearch(arr, mid + 1, r, x)
+
+	else:
+		# Element is not present in the array
+		return -1
+
+
+# Driver Code
+arr = [2, 3, 4, 10, 40]
+x = 10
+
 # Function call
-result = binary_search(arr, 0, len(arr)-1, x)
- 
+result = binarySearch(arr, 0, len(arr)-1, x)
+
 if result != -1:
-    print("Element is present at index", str(result))
+	print("Element is present at index % d" % result)
 else:
-    print("Element is not present in array")
+	print("Element is not present in array")
