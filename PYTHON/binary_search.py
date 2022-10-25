9c44f8915bb6a64a7bd4759ac62c6ce119845725
PrabinDumre <116421340+PrabinDumre@users.noreply.github.com>
2022-10-26 00:14:25 +0530

    Sorting Implementation Using Python

--- PYTHON/binary_search.py
+++ PYTHON/binary_search.py
@@ -1,29 +1,36 @@
-def binary_search(arr, low, high, x):
- 
-    if high >= low:
- 
-        mid = (high + low) // 2
+from sorting import mergesort
 
-        if arr[mid] == x:
-            return mid
 
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
+def search(arr, item):
+    """Performs binary search on an array
+    with the given item and returns True or
+    False.
 
+>>> search([5, 4, 1, 6, 2, 3, 9, 7], 2)
+    True
+
+>>> search([5, 4, 1, 6, 2, 3, 9, 7], 8)
+    False
+    """
+
+    arr = mergesort(arr)
+
+    first = 0
+    last = len(arr) - 1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if arr[midpoint] == item:
+            found = True
         else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        return -1
- 
-# Test array
-arr = [ 2, 3, 4, 10, 40 ]
-x = 3
- 
-# Function call
-result = binary_search(arr, 0, len(arr)-1, x)
- 
-if result != -1:
-    print("Element is present at index", str(result))
-else:
-    print("Element is not present in array")
+            if item < arr[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+    return found
+
+
+print search([5, 4, 1, 6, 2, 3, 9, 7], 2)
+print search([5, 4, 1, 6, 2, 3, 9, 7], 8)
