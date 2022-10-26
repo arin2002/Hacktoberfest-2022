a416b7b2b8c1a211db55e5afaa908ed987e3af1c
Aniket Varma <incredibleaniket2002@gmail.com>
2022-10-26 18:42:45 +0530

    Modifying Valid Sudoku problem with the main function and appropriate test cases

--- Leetcode/valid_sudoku.cpp
+++ Leetcode/valid_sudoku.cpp
@@ -1,3 +1,16 @@
+/*
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+*/
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& b) {
@@ -31,3 +44,44 @@ public:
         return true;
     }
 };
+
+int main(){
+    vector<vector<char>> board;
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cin>>board[i][j];
+        }
+    }
+    Solution *s=new Solution();
+    if(s->isValidSudoku(board)) cout<<"true";
+    else cout<<"false";
+    return 0;
+}
+
+/*
+Test case 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Test case 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+*/
