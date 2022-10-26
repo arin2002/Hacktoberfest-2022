394e126d6a5d033e5cd6d134cdc9caa958e63fc8
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-10-26 18:21:07 +0530

    added java program to count the number of zeroes in a number

---
+++ JAVA/CountZeroes.java
@@ -0,0 +1,24 @@
+public class CountZeroes {
+    public static void main(String[] args) {
+        System.out.println(count(78408300));
+
+    }
+
+    static int count(int n) {
+        return helper(n, 0);
+    }
+
+    static int helper(int n, int c) {
+        if (n == 0) {
+            return c;
+        }
+
+        int rem = n % 10;
+
+        if (rem == 0) {
+            return helper(n / 10, c + 1);
+        } else {
+            return helper(n / 10, c);
+        }
+    }
+}
