769fb947bfc7188d624639c1c28f21bae0049e2a
Sparsh-KING <116799322+Sparsh-KING@users.noreply.github.com>
2022-10-27 15:10:11 +0530

    BIT-FenwickTree

---
+++ C++/BIT-FenwickTree/bit_functions.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+10;
+int bit[N];
+
+void update(int i, int x){
+	for(; i < N; i += (i&-i))
+		bit[i] += x;
+}
+
+int sum(int i){
+	int ans = 0;
+	for(; i > 0; i -= (i&-i))
+		ans += bit[i];
+	return ans;
+}
+
+int main(){
+	
+}
