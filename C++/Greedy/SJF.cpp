5efcc353002bb228b5366b1076000949ca2d7077
Mitanshu Holkar <mitanshuholkar35@gmail.com>
2022-10-28 11:18:02 +0530

    my commit

---
+++ C++/Greedy/SJF.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm> 
+#include <iomanip>
+#include <string.h> 
+using namespace std;
+
+struct process {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+    int response_time;
+};
+
+int main() {
+
+    int n;
+    struct process p[100];
+    float avg_turnaround_time;
+    float avg_waiting_time;
+    float avg_response_time;
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_response_time = 0;
+
+    int is_completed[100];
+    memset(is_completed,0,sizeof(is_completed));
+
+    cout << setprecision(2) << fixed;
+
+    cout<<"Enter the number of processes: ";
+    cin>>n;
+
+    for(int i = 0; i < n; i++) {
+        cout<<"Enter arrival time of process "<<i+1<<": ";
+        cin>>p[i].arrival_time;
+        cout<<"Enter burst time of process "<<i+1<<": ";
+        cin>>p[i].burst_time;
+        p[i].pid = i+1;
+        cout<<endl;
+    }
+
+    int current_time = 0;
+    int completed = 0;
+    int prev = 0;
+
+    while(completed != n) {
+        int idx = -1;
+        int mn = 10000000;
+        for(int i = 0; i < n; i++) {
+            if(p[i].arrival_time <= current_time && is_completed[i] == 0) {
+                if(p[i].burst_time < mn) {
+                    mn = p[i].burst_time;
+                    idx = i;
+                }
+                if(p[i].burst_time == mn) {
+                    if(p[i].arrival_time < p[idx].arrival_time) {
+                        mn = p[i].burst_time;
+                        idx = i;
+                    }
+                }
+            }
+        }
+        if(idx != -1) {
+            p[idx].start_time = current_time;
+            p[idx].completion_time = p[idx].start_time + p[idx].burst_time;
+            p[idx].turnaround_time = p[idx].completion_time - p[idx].arrival_time;
+            p[idx].waiting_time = p[idx].turnaround_time - p[idx].burst_time;
+            p[idx].response_time = p[idx].start_time - p[idx].arrival_time;
+            
+            total_turnaround_time += p[idx].turnaround_time;
+            total_waiting_time += p[idx].waiting_time;
+            total_response_time += p[idx].response_time;
+
+            is_completed[idx] = 1;
+            completed++;
+            current_time = p[idx].completion_time;
+            prev = current_time;
+        }
+        else {
+            current_time++;
+        }
+        
+    }
+
+
+    avg_turnaround_time = (float) total_turnaround_time / n;
+    avg_waiting_time = (float) total_waiting_time / n;
+    avg_response_time = (float) total_response_time / n;
+    cout<<endl<<endl;
+
+    cout<<"#P\t"<<"AT\t"<<"BT\t"<<"ST\t"<<"CT\t"<<"TAT\t"<<"WT\t"<<"RT\t"<<"\n"<<endl;
+
+    for(int i = 0; i < n; i++) {
+        cout<<p[i].pid<<"\t"<<p[i].arrival_time<<"\t"<<p[i].burst_time<<"\t"<<p[i].start_time<<"\t"<<p[i].completion_time<<"\t"<<p[i].turnaround_time<<"\t"<<p[i].waiting_time<<"\t"<<p[i].response_time<<"\t"<<"\n"<<endl;
+    }
+    cout<<"Average Turnaround Time = "<<avg_turnaround_time<<endl;
+    cout<<"Average Waiting Time = "<<avg_waiting_time<<endl;
+    cout<<"Average Response Time = "<<avg_response_time<<endl;
+
+
+}
