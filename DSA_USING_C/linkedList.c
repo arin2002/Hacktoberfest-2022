14ba1c4dbaa99d1f3d178b81ac3368c30571d3da
nian10 <113279627+nian10@users.noreply.github.com>
2022-10-30 20:11:58 +0530

    Added Linked List

---
+++ DSA_USING_C/linkedList.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Represents a node
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    //List of size 0
+    node *list = NULL;
+    
+    //Add number to list
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 1;
+    n->next = NULL;
+    list = n;
+    
+    //Add number to list
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->number = 5;
+    n->next = NULL;
+    list->next = n;
+    
+    //Add number to list
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list->next);
+        free(list);
+        return 1;
+    }
+    n->number = 7;
+    n->next = NULL;
+    list->next->next = n;
+    
+    //Print list
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%i\n", tmp->number);
+    }
+    
+    //Free list
+    while (list != NULL)
+    {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
+    return 0;
+}
