9b194b22830ff856d68ba9d523ff2da0fefb413a
Nian <113279627+coder-nian@users.noreply.github.com>
2023-03-22 21:41:25 +0530

    Create Stack.py

---
+++ PYTHON/Stack.py
@@ -0,0 +1,16 @@
+stack = []
+
+stack.append('x')
+stack.append('y')
+stack.append('z')
+
+print('Initial stack')
+print(stack)
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
