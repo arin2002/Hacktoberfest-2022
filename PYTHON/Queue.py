d51328b4662e9dcd6d150e8915c5f84c075051bc
Nian <113279627+coder-nian@users.noreply.github.com>
2023-03-22 21:45:49 +0530

    Create Queue.py

---
+++ PYTHON/Queue.py
@@ -0,0 +1,16 @@
+queue = []
+
+queue.append('i')
+queue.append('j')
+queue.append('k')
+
+print("Initial queue")
+print(queue)
+
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
